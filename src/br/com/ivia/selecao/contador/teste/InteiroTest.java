dac9a64056d15f4d0259aaa60bc80a5dd40156a5
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-16 21:50:32 -0300

    Testes para classe Inteiro

---
+++ src/br/com/ivia/selecao/contador/teste/InteiroTest.java
@@ -0,0 +1,14 @@
+package br.com.ivia.selecao.contador.teste;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class InteiroTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
